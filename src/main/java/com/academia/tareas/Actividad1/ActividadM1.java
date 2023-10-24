954b3c248dee5d1eb17a72ac8a7edeb63755a4a1
CM-Gimenez <martingimenez2222@gmail.com>
2023-10-23 23:50:25 -0300

    actividad modulo terminada

--- src/main/java/com/academia/tareas/Actividad1/ActividadM1.java
+++ src/main/java/com/academia/tareas/Actividad1/ActividadM1.java
@@ -1,30 +1,40 @@
 package com.academia.tareas.Actividad1;
 
+import java.util.Scanner;
+
 /**
- * Actividad numero 1: Aplicacion que toma el valor numerico de dos variables y
- * calcula la suma.
+ * Actividad numero 1: Aplicacion que toma el valor numerico de dos variables,
+ * calcula la suma e imprime el resultado´por consola.
  *
  * @author Gimenez Carlos Martin.
  */
 public class ActividadM1 {
+    
+    //declaramos e inicializamos una variable Scanner para leer los números a sumar
+    Scanner scann =  new Scanner(System.in);
 
-    //declaracion de variables y le asignamos valores aleatorios mediante la funcion random()
-    int numeroUno = (int) (Math.random() * 1000);
-
-    int numeroDos = (int) (Math.random() * 1000);
-
+    //declaracion de variables
+    int numeroUno;
+    int numeroDos;
+    
+    //variable para guardar el resultado
     int resultado;
+    
+    
+   
 
     //funcion que suma dos numeros aleatorios
     public void suma() {
-        //imprimimos los valores generados 
-        System.out.println("Numero 1: " + numeroUno);
-        System.out.println("Numero 2: " + numeroDos);
+        //le pedimos al usuario que ingrese los numeros a sumar
+        System.out.println("Ingrese el primer número: ");
+        numeroUno = scann.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        numeroDos = scann.nextInt();
 
         //asignamos el valor de la suma a la variable resultado
         resultado = numeroUno + numeroDos;
         
-        //imprimimos el valor de la suma
+        //imprimimos por consola el valor de la suma 
         System.out.println("La suma de ambos numeros es: " + resultado);
 
     }
